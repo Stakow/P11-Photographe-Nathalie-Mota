a36312c796f1423ec386bb6fcf69f0fbad527f55
Lucas Szustakowski <szustakowskilucas@gmail.com>
2024-08-15 14:58:38 +0200

    contact form

--- template-parts/contact.php
+++ template-parts/contact.php
@@ -2,7 +2,8 @@
 <div id="contact-modal" class="modal">
     <div class="modal-content">
         <span id="close-modal" class="close">&times;</span>
-        <?php echo do_shortcode('[contact-form-7 id="votre_id_de_formulaire"]'); ?>
+        <img src="<?php echo get_stylesheet_directory_uri() . '/images/Contact header.png'; ?>" alt="logo contact" class="contact-logo">
+        <?php echo do_shortcode('[contact-form-7 id="f551c53" title="Formulaire de contact 1"]'); ?>
     </div>
 </div>
 
